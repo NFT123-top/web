4964f699d457fb3ed3ed87da0561caf3a0e13853
Andy Woo <surferwu@gmail.com>
2022-03-13 21:58:21 +0800

    add tesing html

--- client/src/commons/Card.jsx
+++ client/src/commons/Card.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 let img = "../../img/logo.svg"
 
-// export default [
-//   {
-//     index: 1,
-//     name: 'OpenSea',
-//     desc: 'dwead ,sw d asdfa dadfas dafsd  asd fasdf',
-//     imgUrl: 'https://opensea.io/static/images/logos/opensea.svg',
-//     hrefUrl: 'https://opensea.io/',
-//     imageAlt: 'OpenSea is the world\'s first and largest NFT marketplace, Discover, collect, and sell extraordinary NFTs.'
-//   },
-// ]
-const button = {
-  className="px-4 py-1 mt-2 text-sm text-blue-600 font-semibold rounded-full border 
-         border-blue-200 hover:text-white hover:bg-blue-600 hover:border-transparent 
-         focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
-}
 
 export default function ({ card }) {
-  // console.log(card);
   return (
-
+    <div className="p-4 bg-white rounded-xl shadow-lg ">
+      <div className="max-w-sm mx-4 flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src={card.imgUrl} alt={card.imageAlt}/>
+        </div>
+        <div>
+          <div className="text-lg font-medium text-black">{card.name}</div>
+          <p className="text-slate-500 text-sm">{card.desc}</p>
+        </div>
+      </div>
+      <div className="flex justify-between mx-4 my-2">
+        {/* <button className="py-1 mt-2 text-sm text-white bg-purple-600 rounded-full hover:text-white 
+        hover:bg-purple-600 hover:border-transparent 
+         focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">HOT</button> */}
+         <a href={card.hrefUrl} target="_blank" >
+           <button className=" px-4 py-1 mt-2 text-sm text-blue-600 font-semibold rounded-full border border-blue-200
+            hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">Enter</button>
+         </a>
+      </div>
+    </div>
+    )
+}
     // <div className="rounded-lg shadow-lg overflow-hidden">
     //   <div className=" flex flex-1 items-center">
     //     <div className="px-2 py-1">
@@ -33,26 +38,7 @@ export default function ({ card }) {
     //     <p>{card.imageAlt}</p>
     //   </div> 
     // </div>
-    <div className="p-4 bg-white rounded-xl shadow-lg ">
-      <div className="max-w-sm mx-auto flex items-center space-x-4">
-        <div className="shrink-0">
-          <img className="h-12 w-12" src={card.imgUrl} alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div className="text-lg font-medium text-black">{card.name}</div>
-          <p className="text-slate-500 text-sm">{card.desc}</p>
-        </div>
-      </div>
-      <div className="flex justify-between mx-8 my-2">
-        {/* <button className="py-1 mt-2 text-sm text-white bg-purple-600 rounded-full hover:text-white 
-        hover:bg-purple-600 hover:border-transparent 
-         focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">HOT</button> */}
-         <a href="" className=""></a>
-        <button onclick="window.location.href={}" >Enter</button>
-      </div>
-    </div>
-    )
-}
+
 // Card type 1
 {/* <a href="https://www.nftworlds.com/" target="_blank" class="flex items-center m-4 gap-2">
   <img class="w-12 h-auto" src="https://s3.coinmarketcap.com/generated/nft/collections/raritysniper-nft-worlds.png" />
--- client/src/components/Tools.jsx
+++ client/src/components/Tools.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import tools from '../data/tools';
-import Card1 from '../commons/Card1';
-import collectionList from '../data/collection';
 
 const ToolsItem = ({ item }) => (
-  <a href={item.herfUrl} type="_blank" className="" >
-    <img className="w-auto h-12" src={item.imgUrl} alt="" />
+  <a href={item.hrefUrl}  target="_blank" className="hover:cursor-pointer" >
+    <img className="w-auto h-12" src={item.imgUrl} alt="123" />
     <span className=" text-gray-700"> {item.name} </span>
   </a>
 );
@@ -14,7 +12,7 @@ const Tools = () => {
   return (
     <div className="mx-4 pt-3">
       <h2 className="text-2xl pb-2 text-gray-900">NFT Tools </h2>
-      <div className="flex gap-6">
+      <div className="flex flex-wrap gap-6">
         {tools.map((item) => (
           <ToolsItem item={item} />
         ))}
