e52b7e03a69826397b5d676c9717f7dad405f2dc
Andy Woo <surferwu@gmail.com>
2022-03-16 09:21:18 +0800

    update svgs

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,35 +6,34 @@ const Footer = () => {
     <footer className="bg-sky-200 w-full py-4 px-8 divide-y-2">
       <div className="flex items-start justify-between gap-8 pb-8">
         <div className="flex flex-col gap-1">
-          <img className="w-12 h-auto" src="../../img/logo-color-sm.svg" alt=""></img> 
+          <img className="w-12 h-auto" src="../../img/logo-color-sm.svg" alt=""></img>
           <span className="text-sm">This is advertisement senctense asd asfa</span>
           <div className="flex flex-row gap-2">
-            <a href="https://twitter.com/TiThinker" target="_blank" ><FaTwitter fontSize={20} className="" onClick={(1===1)}/></a>
-            <a href="https://twitter.com/TiThinker" target="_blank" ><FaDiscord fontSize={20} className="" onClick={(1===1)}/></a>
-             
+            <a href="https://twitter.com/TiThinker" target="_blank" ><FaTwitter fontSize={20} className="" /></a>
+            <a href="https://twitter.com/TiThinker" target="_blank" ><FaDiscord fontSize={20} className="" /></a>
           </div>
         </div>
         <div className="flex flex-row gap-5">
           <div className="grid grid-cols-1 place-content-start gap-2">
-             <span>Market</span> 
-             <span className=" text-gray-600 text-sm">Mdfet</span> 
-             <span className=" text-gray-600 text-sm">Markasdfet</span> 
-             <span className=" text-gray-600 text-sm">Markasdfet</span> 
-             <span className=" text-gray-600 text-sm">Marfet</span> 
+            <span>Market</span>
+            <span className=" text-gray-600 text-sm">Mdfet</span>
+            <span className=" text-gray-600 text-sm">Markasdfet</span>
+            <span className=" text-gray-600 text-sm">Markasdfet</span>
+            <span className=" text-gray-600 text-sm">Marfet</span>
           </div>
           <div className="grid grid-cols-1 place-content-start gap-2">
-             <span>Market</span> 
-             <span className=" text-gray-600 text-sm">Markasdet</span> 
-             <span className=" text-gray-600 text-sm">Markfet</span> 
-             <span className=" text-gray-600 text-sm">Madfet</span> 
-             <span className=" text-gray-600 text-sm">Markasdfet</span> 
+            <span>Market</span>
+            <span className=" text-gray-600 text-sm">Markasdet</span>
+            <span className=" text-gray-600 text-sm">Markfet</span>
+            <span className=" text-gray-600 text-sm">Madfet</span>
+            <span className=" text-gray-600 text-sm">Markasdfet</span>
           </div>
           <div className="grid grid-cols-1 place-content-start gap-2">
-             <span>Market</span> 
-             <span className=" text-gray-600 text-sm"><a href="#">Marsdfakasdfet</a></span> 
-             <span className=" text-gray-600 text-sm">Marsdfet</span> 
-             <span className=" text-gray-600 text-sm">Markasdfet</span> 
-             <span className=" text-gray-600 text-sm">Markasfet</span> 
+            <span>Market</span>
+            <span className=" text-gray-600 text-sm"><a href="#">Marsdfakasdfet</a></span>
+            <span className=" text-gray-600 text-sm">Marsdfet</span>
+            <span className=" text-gray-600 text-sm">Markasdfet</span>
+            <span className=" text-gray-600 text-sm">Markasfet</span>
           </div>
         </div>
       </div>
---
+++ client/src/data/music.js
@@ -0,0 +1,15 @@
+export default[
+  {
+    name: 'OneOf',
+    hrefUrl: 'https://oneof.com',
+    desc: 'The green NFT platform for entertainment. Connecting fans and collectors to their favorite artists, athletes, and brands.',
+    imgUrl: 'https://pbs.twimg.com/profile_images/1488544287746924551/islnGZQP_400x400.png'
+  },
+  {
+    name: '',
+    hrefUrl: '',
+    desc: '',
+    imgUrl: '',
+  },
+
+]
