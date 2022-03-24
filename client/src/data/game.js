0dd29802bef41c8dbcd39fbf2fbd5ae3d4e1e15d
Andy Woo <surferwu@gmail.com>
2022-03-24 21:40:36 +0800

    add new data

---
+++ client/src/data/game.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    name: 'Axie Infinity',
+    hrefUrl: 'http://axieinfinity.com/',
+    desc: 'Battle, Collect, Build Freedom for gamers',
+    imgUrl: 'https://pbs.twimg.com/profile_images/1207674606259187715/60siPMQn_400x400.jpg',
+  },
+]
